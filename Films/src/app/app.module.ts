6a469370be59af71365dfbfba118d5fb828e2e95
Lautaro Ruiz <lautaroruiz04@gmail.com>
2023-10-29 16:16:02 -0300

    Comenzando CRUD User en su servicio

--- Films/src/app/app.module.ts
+++ Films/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmsFromAPIService } from './services/films-from-api.service';
 import { TestComponent } from './test/test.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HttpClient} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
--- Films/src/app/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    nombre: String,
-    apellido: String,
-    email: String,
-    password: String, 
-    addres: String,
-    dni: String,
-    userId: Number
-}
