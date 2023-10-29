aa3b56447abdf4fdeb7c776020c5e8130c05ce59
Lautaro Ruiz <lautaroruiz04@gmail.com>
2023-10-29 16:42:22 -0300

    address

--- Films/src/app/components/register/register.component.ts
+++ Films/src/app/components/register/register.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { OnInit, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {User } from 'src/app/models/user';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
+  users = new Array<User>()
+  
   userForm = new FormGroup ({
     firstName: new FormControl (''),
     lastName: new FormControl (''),
     email: new FormControl (''),
-    adress: new FormControl (''),
+    address: new FormControl (''),
     password: new FormControl (''),
     DNI: new FormControl ('', [Validators.minLength(8), Validators.maxLength(8)])
   })
@@ -22,7 +25,7 @@ export class RegisterComponent implements OnInit {
   constructor (private userService: UserService){}
   
   ngOnInit (){
-
+    this.userService.getUsers ().subscribe ((users) => this.users=users)
   }
 
   get firstName() { return this.userForm.get('firstName'); }
@@ -30,11 +33,15 @@ export class RegisterComponent implements OnInit {
   get dni() { return this.userForm.get('DNI'); }
   get email() { return this.userForm.get('email'); }
   get address() { return this.userForm.get('address'); }
+  private get password() { return this.userForm.get('password'); }
   
   onSubmit (){
     let user = new User ()
-
-    
+    user.nombre = this.firstName.value;
+    user.apellido = this.lastName.value;
+    user.email = this.email.value;
+    user.dni = this.dni.value;
+    user.password = this.password.value;
   }
 
 } 
