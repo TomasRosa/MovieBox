fed239d6d269b18c3963c268ddc6a76c3c7fb632
JoelLucasGomez <joellucas2744@gmail.com>
2023-10-31 13:32:03 -0300

    Aside menu e incluido login registro y cobre nosotros

--- Films/src/app/app-routing.module.ts
+++ Films/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path: 'inicio', component:InicioComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registrarse', component: RegisterComponent},
   {path: 'sobre-nosotros', component: SobreNosotrosComponent},
 ];
 
