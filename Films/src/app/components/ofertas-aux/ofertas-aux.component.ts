60f899dbf3885fdaba687d93a2f72407788e2767
JoelLucasGomez <joellucas2744@gmail.com>
2023-11-06 17:41:11 -0300

    ofertas

--- Films/src/app/components/ofertas-aux/ofertas-aux.component.ts
+++ Films/src/app/components/ofertas-aux/ofertas-aux.component.ts
@@ -3,14 +3,12 @@ import { Film } from 'src/app/models/film';
 import { FilmsFromAPIService } from 'src/app/services/films-from-api.service';
 
 @Component({
-  selector: 'app-inicio-aux',
-  templateUrl: './inicio-aux.component.html',
-  styleUrls: ['./inicio-aux.component.css']
+  selector: 'app-ofertas-aux',
+  templateUrl: './ofertas-aux.component.html',
+  styleUrls: ['./ofertas-aux.component.css']
 })
-
-export class InicioAuxComponent implements OnInit{
-  films: any;
-  preciosGenerados: boolean = false;
+export class OfertasAuxComponent implements OnInit {
+  private films: any;
   filteredFilms: any[] = [];
 
   constructor(private dataFilms: FilmsFromAPIService) {}
@@ -18,48 +16,58 @@ export class InicioAuxComponent implements OnInit{
   async ngOnInit(): Promise<void> {
     await this.dataFilms.initializeData();
     this.films = this.dataFilms.getMovies();
-    this.filteredFilms = this.films.filter((film: Film) => film.precio <= 1500);
-    this.mostrarPeliculasRandom();
+    this.filteredFilms = this.films.filter((film: Film) => film.precio > 1500);
+    this.mostrarPeliculasEnOferta();
   }
 
-  mostrarPeliculasRandom() {
+  mostrarPeliculasEnOferta() {
     if (this.filteredFilms) 
     {
       const tabla = document.createElement("table");
-      const contenedorTabla = document.getElementById("tablaPeliculas");
-    
+      const contenedorTabla = document.getElementById("peliculasEnOferta");
+  
       let tbody = document.createElement("tbody");
       tabla.appendChild(tbody);
-    
+  
       for (let i = 0; i < this.filteredFilms.length; i++) {
-          if (i % 5 === 0) {
+        if (i % 5 === 0) {
             let fila = document.createElement("tr");
-    
+            let nuevaFila = document.createElement ("tr");
+  
             for (let j = 0; j < 5; j++) {
               if (i + j >= this.filteredFilms.length) {
                 break;
               }
-    
+
+              let precioAnterior = this.filteredFilms[i + j].precio;
+  
               const celda = document.createElement("td");
               celda.innerHTML = `
                 <img alt="Imagen no disponible" src="${this.filteredFilms[i + j].image}" width="200" height="300">
                 <i><b style="font-size: 15px;">${this.filteredFilms[i + j].title}</b></i>
-                <i><b style="font-size: 15px;">$${this.filteredFilms[i + j].precio}</b></i>
+                <i><b style="font-size: 15px;"><s>${precioAnterior}</s></b></i>
               `;
+
+              const nuevaCelda = document.createElement("td");
+              nuevaCelda.innerHTML = `
+                <i><b style="font-size: 15px;" class = "classNuevoPrecio">Nuevo precio: ${this.filteredFilms[i + j].precio/2}</b></i>
+              `;
+
               fila.appendChild(celda);
+              nuevaFila.appendChild (nuevaCelda);
             }
-    
+  
             tbody.appendChild(fila);
+            tbody.appendChild (nuevaFila);
           }
-      }
-    
-      if (contenedorTabla) {
-        contenedorTabla.appendChild(tabla);
-      } else {
-        console.error("No se encontró el contenedor de la tabla.");
+          if (contenedorTabla) {
+            contenedorTabla.appendChild(tabla);
+          } else {
+            console.error("No se encontró el contenedor de la tabla.");
+          }
       }
     } else {
       console.error("No se encontraron datos de películas.");
     }
-  }    
+  }
 }
--- Films/src/app/components/ofertas/ofertas.component.ts
+++ Films/src/app/components/ofertas/ofertas.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmsFromAPIService } from 'src/app/services/films-from-api.service';
 
 @Component({
   selector: 'app-ofertas',
   templateUrl: './ofertas.component.html',
   styleUrls: ['./ofertas.component.css']
 })
-export class OfertasComponent {
+export class OfertasComponent
+{
+  
+}  
 
-}
