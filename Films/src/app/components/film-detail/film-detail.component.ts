ebe6710b01a9c7d3e9a02460d26136a5e9a78342
Tomas Rosa <trosa@emmsa.net>
2024-10-13 17:31:37 -0300

    feature: seccion de reseñas agregada, logica totalmente funcional en local storage

--- Films/src/app/components/film-detail/film-detail.component.ts
+++ Films/src/app/components/film-detail/film-detail.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FilmsFromAPIService } from 'src/app/services/films-from-api.service';
 import { Film } from 'src/app/models/film';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-film-detail',
@@ -13,15 +15,28 @@ export class FilmDetailComponent {
   private filmRank: number = 0;
   movie: Film | undefined;
 
-  constructor(private route: ActivatedRoute, private films: FilmsFromAPIService) {}
+  newReview: string = ''; // Para almacenar la nueva reseña
 
-  ngOnInit(): void
-  {
+  userActual: User | null = null;
+  isLoggedIn: boolean = false;
+  errorResena: string = '';
+
+
+  constructor(private route: ActivatedRoute, 
+  private films: FilmsFromAPIService,
+  private userService: UserService) {}
+
+  ngOnInit(): void {
     this.films.initializeData().then(() => {
       this.arrayFilms = this.films.getMovies();
       this.getFilmRank();
     });
+  
+    // Obtener el usuario actual
+    this.userActual = this.userService.getUserActual();
+    this.isLoggedIn = this.userActual !== null;
   }
+  
 
   getFilmRank() {
     this.route.paramMap.subscribe(params => {
@@ -30,29 +45,59 @@ export class FilmDetailComponent {
       if (rankParam !== null) {
         this.filmRank = +rankParam;
         this.movie = this.searchFilmWithRank();
+        this.loadReviews(); // Cargar reseñas desde localStorage
       } else {
         console.log('No se encontró el parámetro "rank".');
       }
     });
   }
 
-  searchFilmWithRank (): Film | undefined
-  {
-    return this.arrayFilms.find(film => film.rank === this.filmRank);
+  searchFilmWithRank(): Film | undefined {
+    const foundFilm = this.arrayFilms.find(film => film.rank === this.filmRank);
+    
+    if (foundFilm && !foundFilm.reviews) {
+      foundFilm.reviews = []; // Asegurarse de que el array de reseñas esté inicializado
+    }
+  
+    return foundFilm;
   }
 
-  // showFilm ()
-  // {
-  //   let image = document.getElementById ("film")
-  //   this.movie = this.searchFilmWithRank();
+  addReview() {
+    if (!this.isLoggedIn) {
+      this.errorResena = 'Debes iniciar sesión para realizar una reseña';
+      setTimeout(() => {
+        this.errorResena = '';
+      }, 2000);
+      return;
+    }
+  
+    if (this.movie && this.newReview) {
+      if (!this.movie.reviews) {
+        this.movie.reviews = [];
+      }
+      
+      // Agregar nombre y apellido del usuario a la reseña
+      const userName = `${this.userActual?.firstName} ${this.userActual?.lastName}`;
+      const reviewWithUser = `${userName}: ${this.newReview}`;
+      
+      this.movie.reviews.push(reviewWithUser);
+      this.saveReviews(); // Guardar las reseñas en localStorage
+      this.newReview = ''; // Limpiar el campo de entrada
+    }
+  }
+  
+  saveReviews() {
+    if (this.movie) {
+      localStorage.setItem(`reviews_${this.movie.rank}`, JSON.stringify(this.movie.reviews));
+    }
+  }
 
-  //   if (image && this.movie)
-  //   {
-  //     image.innerHTML = `
-  //       <img alt="Imagen no disponible" src="${this.movie.image}" width="200" height="300">
-  //     `
-  //   }
-  // }
-
+  loadReviews() {
+    if (this.movie) {
+      const storedReviews = localStorage.getItem(`reviews_${this.movie.rank}`);
+      if (storedReviews) {
+        this.movie.reviews = JSON.parse(storedReviews);
+      }
+    }
+  }
 }
-
--- Films/src/app/models/film.ts
+++ Films/src/app/models/film.ts
@@ -1,15 +1,16 @@
 export class Film {
-    "rank": number=0;
-    "title": string='';
-    "description": string='';
-    "image": string='';
-    "big_image": string='';
-    "genre": string[]=[];
-    "thumbnail": string='';
-    "rating": number=0;
-    "id": string='';
-    "year": number=0;
-    "imdib": string='';
-    "imdb_link": string='';
-    "precio": number = 0;
+    rank: number = 0;
+    title: string = '';
+    description: string = '';
+    image: string = '';
+    big_image: string = '';
+    genre: string[] = [];
+    thumbnail: string = '';
+    rating: number = 0;
+    id: string = '';
+    year: number = 0;
+    imdb: string = '';
+    imdb_link: string = '';
+    precio: number = 0;
+    reviews?: string[]; // Se marca como opcional, ya que no siempre viene de la API
 }
