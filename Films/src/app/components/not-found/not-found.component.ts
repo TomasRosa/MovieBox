ca955caf1fb3002e77527757c035c0f8ae519c63
Lautaro Ruiz <lautaroruiz04@gmail.com>
2024-11-22 14:31:20 -0300

    style: modifique el estilo del not found y redirije al inicio ahora luego de 2.5s

--- Films/src/app/components/not-found/not-found.component.ts
+++ Films/src/app/components/not-found/not-found.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
 
+  constructor (private routerService: Router){}
+
+  ngOnInit(): void {
+    setTimeout (()=>{
+      this.routerService.navigate (['/inicio'])
+    }, 2500)    
+  }
 }
